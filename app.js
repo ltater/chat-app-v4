472a7e267bd45dcf81ebca780e11c71b3ad280c2
Larissa Tater <larissa.tater@gmail.com>
2015-12-07 11:54:45 -0600

    server side api pulls random title and url

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var mongoose = require('mongoose');
+var request = require('request');
 var users = {};
 
 server.listen(3000);
 
 //Mongoose Connection
 mongoose.connect(process.env.MONGO_DB_CONN_CHATAPPS);
+
+var NYT_API_KEY = process.env.NYT_API_KEY;
 
 //Document Schema
 var chatSchema = mongoose.Schema({
@@ -72,11 +75,32 @@ io.sockets.on('connection', function(socket) {
 				if(err) throw err; 
 				//send to everyone including the person who sent the message
 				io.sockets.emit('new message', {msg: msg, nick: socket.nickname});
+				var apiCall = "http://api.nytimes.com/svc/topstories/v1/sports.json?api-key=" + NYT_API_KEY;
+				console.log(apiCall);
+
+				request(apiCall, function(err, resp, object) {
+					var object = JSON.parse(object);
+					var articles = object.results;
+					var usedArticle = [];
+
+					function findTitles(articles) {
+						var randomArticle = articles[Math.floor(Math.random()*articles.length)];
+						var randomArticleTitle = randomArticle.title;
+						var randomArticleURL = randomArticle.url;
+						console.log(randomArticleTitle);
+						console.log(randomArticleURL);
+					}
+
+					findTitles(articles);
+					// console.log(usedArticle);
+				});
 				// send to everyone but the person who sent the message
 				//socket.broadcast.emit('new message', data);
 			});
 		}
 	});
+
+
 
 	socket.on('disconnect', function(data) {
 		if(!socket.nickname) return;
